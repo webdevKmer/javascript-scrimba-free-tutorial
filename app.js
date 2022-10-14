583034ac2e68a0b037d5515097b7d6e60e9e34c0
webdevKmer <webdevKmer@gmail.com>
2022-10-14 13:50:58 +0000

    coding the functionnality of the app

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
-let appleShelf = document.getElementById("apple-shelf")
-let orangeShelf = document.getElementById("orange-shelf")
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊", "🍎", "🍎", "🍊", "🍎", "🍎", "🍊", "🍊", "🍊", "🍊"]
+const appleShelf = document.getElementById("apple-shelf")
+const orangeShelf = document.getElementById("orange-shelf")
 
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
 
+function sortFruits(){
+    let oranges = ""
+    let apples = ""
+    for (i = 0; i < fruit.length; i++){
+        if (fruit[i] === "🍎"){
+            apples += fruit[i]
+            console.log('putting 1 apple in the shelf')
+        } else {
+            oranges += fruit[i]
+            console.log('putting 1 orange in the shelf')
+        }
+    }
+    appleShelf.textContent = apples
+    orangeShelf.textContent = oranges
+}
+
+sortFruits()
