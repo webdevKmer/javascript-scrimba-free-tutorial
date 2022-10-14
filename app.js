df91310e7f18639340bb3e79ce3c9f2d87215d11
webdevKmer <webdevKmer@gmail.com>
2022-10-14 15:59:47 +0000

    finishing the option for numbers and symbols

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+// To do
+// Add functionnality "Copy on click"
+// Set password length
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 let availableCharacters = []
-let addNumbers = true
-let addSymbols = true
 
 const passwordLength = 15
 
@@ -13,13 +15,18 @@ const genPasswordBtn = document.getElementById('gen-passwords')
 const password1Elt = document.getElementById('password1')
 const password2Elt = document.getElementById('password2')
 
+const numbersCheckbox = document.getElementById('numbers')
+const symbolsCheckbox = document.getElementById('symbols')
+
+console.log(symbolsCheckbox.checked, numbersCheckbox.checked)
+
 function getCharacters() {
-    if(addSymbols && addNumbers) {
+    if(numbersCheckbox.checked && symbolsCheckbox.checked) {
         availableCharacters = characters.concat(symbols).concat(numbers)
-    } else if(addSymbols) {
-        availableCharacters = characters.concat(symbols)
-    } else if (addNumbers){
+    } else if(numbersCheckbox.checked) {
         availableCharacters = characters.concat(numbers)
+    } else if (symbolsCheckbox.checked){
+        availableCharacters = characters.concat(symbols)
     } else {
         availableCharacters = characters
     }
