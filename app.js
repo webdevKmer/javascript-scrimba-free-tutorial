c3034159ada86e84aa78c98f709b27de5bccc342
webdevKmer <webdevKmer@gmail.com>
2022-10-14 13:35:05 +0000

    adding functionnality to the app.

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ fightButton.addEventListener("click", function() {
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+
+    let player1 = fighters[Math.floor(Math.random()*fighters.length)]
+    let player2 = fighters[Math.floor(Math.random()*fighters.length)]
+
+    stageEl.textContent = player1 + " VS " + player2
 })
